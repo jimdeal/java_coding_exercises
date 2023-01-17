b0e26096d8bc89786f97e2924bb8aeaf5ccabc3d
jimdealuk <jimdeal@hotmail.com>
2023-01-17 09:05:05 +0000

    Update tests - WIP

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,7 +1,34 @@
 package com.techreturners.exercise002;
 
-public class Exercise002 {
+class Person {
+    private String pFName;
+    private String pSName;
+    private String pCity;
+    private Integer pAge;
 
+    public Person(String firstName, String surname, String city, Integer age) {
+        this.pFName = firstName;
+        this.pSName = surname;
+        this.pCity = city;
+        this.pAge = age;
+    }
+
+    public String getFirstName() {
+        return pFName;
+    }
+    public String getSurname(){
+        return pFName;
+    }
+    public String getCity() {
+        return pCity;
+    }
+    public int getAge(){
+        return pAge;
+    }
+}
+
+
+public class Exercise002 {
     public boolean isFromManchester(Person person) {
         return person.getCity().compareTo("Manchester")==0;
     }
